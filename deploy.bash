a9dbec80532aa7d016fe3e735e4e5acee78d1e37
khai96_ <hvksmr1996@gmail.com>
2025-02-18 16:41:04 +0700

    compare

--- deploy.bash
+++ deploy.bash
@@ -3,4 +3,5 @@ set -o errexit -o pipefail -o nounset
 cd "$(dirname "$0")"
 pretty-exec -- rm -rf output
 pretty-exec -- pnpm deploy --filter=foo ./output
+pretty-exec -- lsd -lA packages/foo
 pretty-exec -- lsd -lA output
